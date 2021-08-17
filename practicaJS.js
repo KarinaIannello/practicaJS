08fcc5216d63013a0040e049b241bca5c458cbf5
KarinaIannello <kari.iannello.ki@gmail.com>
2021-08-16 23:46:28 -0300

    primer commit

---
+++ practicaJS.js
@@ -0,0 +1,61 @@
+//cepillarse los dientes
+
+//comprobamos si tenemos cepillo
+let tienesCepillo = true;
+console.log(tienesCepillo);
+
+//comprobar si tenemos pasta de dientes
+let tienesPasta = true;
+console.log(tienesPasta);
+
+//comprobar si comiste
+let comida = true;
+console.log(comida);
+
+//horario donde te cepillas siempre
+const horaFija = "08:30";
+console.log(horaFija);
+
+//hora actual
+let horaActual = "09:10";
+console.log(horaActual);
+
+//tiepo que tardas en cepillarte
+let tiempoCepillado = "2 minutos";
+console.log(tiempoCepillado);
+
+//cantidad de cepilaldas
+let cantidadCepilladas = 45;
+console.log(cantidadCepilladas);
+
+
+//Segundo ejercicio
+
+//Irse a dormir
+
+//te cepillaste los dientes 
+let cepillarte = true;
+
+//cenaste
+let cenaste = false;
+
+//programar despertador
+const despertar = "08:05 a.m.";
+
+//cuantas luces apagaste
+let numeroLuces = 3;
+
+//cantidad de horas que dormiste
+let horas = 8;
+
+//cantidad de frazadas
+let cantidadFrazadas = 4;
+
+//cantidad de almohadas
+let cantidadAlmohadas = 2;
+
+let dormir = [
+    cepillarte, cenaste, despertar, numeroLuces, horas, cantidadFrazadas, cantidadAlmohadas
+]
+
+console.log(dormir);
