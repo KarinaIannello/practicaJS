a93117c0574e85eb7eff2588d8f149e9c9224d66
KarinaIannello <kari.iannello.ki@gmail.com>
2021-08-17 16:15:28 -0300

    cambio nombre repo y archivo, agrego archivo operadores

---
+++ operadores.js
@@ -0,0 +1,30 @@
+//OPERADORES MATEMATICOS
+    // +, -, *, /, %
+    //INCREMENTO
+let numero = 6;
+console.log(++numero);
+    //DECREMENTO
+console.log(--numero);
+
+//Comparacion simple, ==
+console.log(5 == "5");
+
+//Comparacion estricta, ===
+console.log(5 ==="5");
+
+//Desigualdad simple, !=
+console.log(5 != "5");
+
+//Desigualdad estricta, !==
+
+//Menor o igual, <=
+
+//OPERADORES LOGICOS
+
+//And, &&
+
+//OR, \\
+
+//Negacion, !
+
+//Concatencaion, +
